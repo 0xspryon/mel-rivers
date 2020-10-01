aa33fa93a574da7d9e9f110352170c1862536e58
Yonga Springfield <springfieldyonga@outlook.com>
2020-10-01 13:32:25 +0100

    Updates

---
+++ precache-manifest.07773cd78983009c3e5fd27d118eff90.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e974fb255544d3e0f773cadf9e225478",
+    "url": "/mel-rivers/index.html"
+  },
+  {
+    "revision": "8a43c4ec6469e89fa315",
+    "url": "/mel-rivers/static/css/main.412f7ad3.chunk.css"
+  },
+  {
+    "revision": "001c3f1cc238a7f0a1ba",
+    "url": "/mel-rivers/static/js/2.1768474f.chunk.js"
+  },
+  {
+    "revision": "10017bfb08c3f52cf1c70a46d09c7c96",
+    "url": "/mel-rivers/static/js/2.1768474f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a43c4ec6469e89fa315",
+    "url": "/mel-rivers/static/js/main.10a3b99b.chunk.js"
+  },
+  {
+    "revision": "89aa707bbe70eeec4092",
+    "url": "/mel-rivers/static/js/runtime-main.488c57f6.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/mel-rivers/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/mel-rivers/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/mel-rivers/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/mel-rivers/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "d6c4e78d7090519fda9f11d208356f79",
+    "url": "/mel-rivers/static/media/up-arrow-circle.d6c4e78d.svg"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/mel-rivers/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
